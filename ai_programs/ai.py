2b0658a311ed8bb6e359890928e0d762700de0b9
ROBOT <tkata12@outlook.jp>
2024-09-18 16:45:14 +0900

    基礎ファイルを作成

---
+++ ai_programs/ai.py
@@ -0,0 +1,8 @@
+import torch
+
+class AiClient:
+    def __init__(self):
+        pass
+    def createModel(self):
+        pass
+ 
--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 import torch
 
+from ai_programs.ai import AiClient
+
 def main():
-    pass
+    ai = AiClient()
+    print(torch.__version__)
 
 if __name__ == "__main__":
     main()
