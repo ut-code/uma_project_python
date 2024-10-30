142fc474ff8b17c6514dc83bc94ce824f55f03d7
ROBOT <tkata12@outlook.jp>
2024-10-31 00:33:37 +0900

    試合ごとの予想データを追加

--- index.py
+++ index.py
@@ -1,97 +1,43 @@
 import json
+import os
 
 from ai_programs.ai import AiClient
 
-
 def main():
     ai = AiClient("v4")
-    predictions = ai.prediction_rank({
-        "title": "サウジアラビアロイヤルカップ",
-        "course": {
-            "distance": 1600,
-            "surface": "芝",
-            "direction": "右",
-            "position": "外"
-        },
-        "url": "https://www.jra.go.jp/datafile/seiseki/g1/afs/result/afs2023.html",
-        "horse": [
-            {
-                "waku": "枠1白",
-                "umaban": 1,
-                "horse": "アルテヴェローチェ",
-                "age": "牡2",
-                "weight": "56.0",
-                "jockey": "佐々木 大輔",
-                "h_weight": 448,
-                "pop": "1"
-            },
-            {
-                "waku": "枠2黒",
-                "umaban": 2,
-                "horse": "ニシルノアルノーヴァ",
-                "age": "牡2",
-                "weight": "56.0",
-                "jockey": "永野 猛蔵",
-                "h_weight": 470,
-                "pop": "5"
-            },
-            {
-                "waku": "枠3赤",
-                "umaban": 3,
-                "horse": "アルレッキーノ",
-                "age": "牡2",
-                "weight": "56.0",
-                "jockey": "C.ルメール",
-                "h_weight": 462,
-                "pop": "1"
-            },
-            {
-                "waku": "枠4青",
-                "umaban": 4,
-                "horse": "マイネルチケット",
-                "age": "牡2",
-                "weight": "56.0",
-                "jockey": "戸崎 圭太",
-                "h_weight": 454,
-                "pop": "2"
-            },
-            {
-                "waku": "枠5黄",
-                "umaban": 5,
-                "horse": "タイセイカレント",
-                "age": "牡2",
-                "weight": "56.0",
-                "jockey": "横山 武史",
-                "h_weight": 486,
-                "pop": "5"
-            },
-            {
-                "waku": "枠6緑",
-                "umaban": 6,
-                "horse": "シンフォーエバー",
-                "age": "牡2",
-                "weight": "56.0",
-                "jockey": "菅原 明良",
-                "h_weight": 496,
-                "pop": "2"
-            },
-            {
-                "waku": "枠7橙",
-                "umaban": 7,
-                "horse": "フードマン",
-                "age": "牡2",
-                "weight": "56.0",
-                "jockey": "松山 弘平",
-                "h_weight": 462,
-                "pop": "1"
-            },
-        ]
-    })
+    files = os.listdir("db/jra")
+    for f in files:
+        loaded = load_data(f"db/jra/{f}")
+        data = {
+            "title": loaded["title"],
+            "course": loaded["course"],
+	        "horse": [],
+        }
+        for horse in loaded["horse"]:
+            data["horse"].append({
+                "horse": horse["horse"],
+                "age": horse["age"],
+                "jockey": horse["jockey"],
+                "pop": horse["pop"],
+                "weight": horse["weight"],
+                "h_weight": horse["h_weight"],
+                "waku": horse["waku"],
+                "umaban": horse["umaban"],
+            })
+        predictions = {
+            "title": loaded["title"],
+            "url": loaded["url"],
+            "horse": ai.prediction_rank(data)
+        }
 
-    for horse in predictions:
-        name = horse["name"]
-        rank = horse["rank"]
-        print(f"{name}: {rank}")
+        write_file(f"db/predictions/{f}", json.dumps(predictions, indent=4, ensure_ascii=False))
+
+def load_data(file_path: str):
+    with open(file_path, "r", encoding="UTF-8") as f:
+        return json.loads(f.read())
+def write_file(file_path: str, content: str):
+    with open(file_path, "w", encoding="UTF-8") as f:
+        return f.write(content)
 
 if __name__ == "__main__":
     main()
